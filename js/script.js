e113b038089d2f8748668b926af7bae1d6ded4e7
KaterinaLove <KaterinaLove@users.noreply.github.com>
2020-09-26 01:50:53 +0300

    Lesson 1

--- js/script.js
+++ js/script.js
@@ -17,55 +17,43 @@ const GOODS = [
   },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+
+but.onclick = function () {
+ for (let i = 0; i < GOODS.length; i++) {
+   let div = document.createElement('div');
+   div.className = "goods-list";
+   div.innerHTML = '<h3 class="title">' + GOODS[i].title +
+     '</h3><p class="price">' + GOODS[i].price + '</p>';
+   main.appendChild(div);
+ }
 };
-
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+/*
+//генирирует часть будующего массива
+const renderGoodsItem = (title, price) => {
+ return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+const renderGoodsList = () => {
+ //map функция перебора исоздания нового массива
+ //goodsList - название нового массива
+ //GOODS - массив который мы перебираем
+ //item - переменная стрелочной функции принимает значения от 0 до goods.length
+ //renderGoodsItem - вызываемая функция в которую передается goods[item].title и записывается в goodsList
+ let goodsList = GOODS.map(item => renderGoodsItem(item.title, item.price));
+ //querySelector - вернется первый элемент в документе с классом goods-list
+ //Выводятся запятые потому, что мы вывадим массив, а разделителем в массиве ясляется ','
+ document.querySelector('.goods-list').innerHTML = goodsList;
 }
+renderGoodsList();
+*/
 
-renderGoodsList(GOODS);
-
-
-//
-//but.onclick = function () {
-//  for (let i = 0; i < GOODS.length; i++) {
-//    let div = document.createElement('div');
-//    div.className = "goods-list";
-//    div.innerHTML = '<h3 class="title">' + GOODS[i].title +
-//      '</h3><p class="price">' + GOODS[i].price + '</p>';
-//    main.appendChild(div);
-//  }
-//};
-///*
-////генирирует часть будующего массива
-//const renderGoodsItem = (title, price) => {
-//  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-//};
-//const renderGoodsList = () => {
-//  //map функция перебора исоздания нового массива 
-//  //goodsList - название нового массива
-//  //GOODS - массив который мы перебираем
-//  //item - переменная стрелочной функции принимает значения от 0 до goods.length
-//  //renderGoodsItem - вызываемая функция в которую передается goods[item].title и записывается в goodsList
-//  let goodsList = GOODS.map(item => renderGoodsItem(item.title, item.price));
-//  //querySelector - вернется первый элемент в документе с классом goods-list
-//  //Выводятся запятые потому, что мы вывадим массив, а разделителем в массиве ясляется ','
-//  document.querySelector('.goods-list').innerHTML = goodsList;
-//}
-//renderGoodsList();
-//*/
-//
-////В этом примере убрала функцию генирирует часть будующего массива
-////перенесла ее в стрелочную функцию item и уменьнила код тем самым на 3 строки
-////Дабавила значения по умолчанию для аргументов функции (list = GOODS)
-////join('') - выводит массив с указанным разделителем
-//const renderGoodsList = (list = GOODS) => {
-//  let goodsList = list.map(
-//    item => `<div class="goods-item"><h3>${item.title}</h3><p>${item.price}</p></div>`
-//  );
-//  document.querySelector('.goods-list').innerHTML = goodsList.join('');
-//}
-//renderGoodsList();
+//В этом примере убрала функцию генирирует часть будующего массива
+//перенесла ее в стрелочную функцию item и уменьнила код тем самым на 3 строки
+//Дабавила значения по умолчанию для аргументов функции (list = GOODS)
+//join('') - выводит массив с указанным разделителем
+const renderGoodsList = (list = GOODS) => {
+ let goodsList = list.map(
+   item => `<div class="goods-item"><h3>${item.title}</h3><p>${item.price}</p></div>`
+ );
+ document.querySelector('.goods-list').innerHTML = goodsList.join('');
+}
+renderGoodsList();
